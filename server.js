16c6681a7a94eec31a9c856b1c4ef4e5b74b60d4
gliechty <guy.liechty@gmail.com>
2016-12-01 11:34:34 -0700

    finished sprint 1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
-
+var db = require('./models');
 
 /////////////////////////////
 //  SETUP and CONFIGURATION
@@ -8,7 +8,7 @@
 
 //require express in our app
 var express = require('express'),
-  bodyParser = require('body-parser');
+   bodyParser = require('body-parser');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -25,29 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
+
 
 
 
@@ -59,9 +37,6 @@ var books = [
 //  ROUTES
 ///////////////////
 
-
-
-
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
@@ -69,22 +44,29 @@ app.get('/', function (req, res) {
 
 // get all books
 app.get('/api/books', function (req, res) {
+  db.Book.find({}, function (err, books) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+    if (err) { return console.log("index error: " + err); }
+    res.json(books);
+  });
 });
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
-  // find one book by its id
-  console.log('books show', req.params);
-  for(var i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
+  db.Book.findById(Books._id, function (err, book){
+      // find one book by its id
+    if (err) {return console.log("id error: "+ err); }
+    res.json(book);
+  });
 });
+
+  // console.log('books show', req.params);
+  // for(var i=0; i < books.length; i++) {
+  //   if (books[i]._id === req.params.id) {
+  //     res.json(books[i]);
+  //     break; // we found the right book, we can stop searching
+  //   }
+  // }
 
 // create new book
 app.post('/api/books', function (req, res) {
